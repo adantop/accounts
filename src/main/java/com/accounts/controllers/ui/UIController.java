56e7b6f73d234509c737fee1f203a389df7a5d69
adantop <40446785+adantop@users.noreply.github.com>
2019-01-22 20:43:11 -0600

    Added homepage
    Added homepage and removed dependency to mysql and added h2

---
+++ src/main/java/com/accounts/controllers/ui/UIController.java
@@ -0,0 +1,13 @@
+package com.accounts.controllers.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UIController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
